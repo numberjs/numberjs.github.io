947dd00202382be04f2846d4c6a7ec4f7f257c9d
Rob <rob@dragonwrench.com>
2015-12-24 10:47:31 +0000

    adding in more themes forces my hand to add a shawdow box to make light themes pop out more

--- js/main.js
+++ js/main.js
@@ -1,30 +1,29 @@
 (function() {
   var stylelist = function() {
     var id = 'current_theme';
-    var themes = [
-      "atelier-cave-dark",
-      "atelier-dune-dark",
-      "atelier-estuary-dark",
-      "atelier-forest-dark",
-      "atelier-heath-dark",
-      "atelier-lakeside-dark",
-      "atelier-plateau-dark",
-      "atelier-savanna-dark",
-      "atelier-seaside-dark",
-      "atelier-sulphurpool-dark",
-      "dark",
-      "darkula",
-      "ir-black",
-      "kimbie.dark",
-      "paraiso-dark",
-      "railscasts",
-      "solarized-dark",
-    ]
-    
+    var themes = ["agate", "atelier-forest-dark", "atelier-seaside-dark", "docco", "paraiso-dark", "tomorrow-night-blue", "androidstudio", "atelier-forest-light", "atelier-seaside-light", "far", "ir-black", "paraiso-light", "tomorrow-night-bright", "arta", "atelier-heath-dark", "atelier-sulphurpool-dark", "foundation", "pojoaque", "tomorrow-night", "ascetic", "atelier-heath-light", "atelier-sulphurpool-light", "github", "kimbie.dark", "railscasts", "tomorrow-night-eighties", "atelier-cave-dark", "atelier-lakeside-dark", "brown-paper", "github-gist", "kimbie.light", "rainbow", "vs", "atelier-cave-light", "atelier-lakeside-light", "codepen-embed", "googlecode", "magula", "xcode", "atelier-dune-dark", "atelier-plateau-dark", "color-brewer", "grayscale", "mono-blue", "solarized-dark", "zenburn", "atelier-dune-light", "atelier-plateau-light", "dark", "hopscotch", "monokai", "solarized-light", "atelier-estuary-dark", "atelier-savanna-dark", "darkula", "hybrid", "monokai-sublime", "sunburst", "atelier-estuary-light", "atelier-savanna-light", "default", "idea", "obsidian", "tomorrow"];
+
+    var add_or_hide_shadow = function() {
+      var darkness_ratio = $('.hljs').css('background-color')
+                                        .replace(/rgba?\((\d+, \d+, \d+)\).*/, "$1")
+                                        .split(/\s*,/)
+                                        .reduce(function(s, c) { return s + parseFloat(c) }, 0)/3;
+      
+/*
+      console.log($('.hljs').css('background'), $('.hljs').css('background-color'));
+      console.log(darkness_ratio);
+      if(darkness_ratio > 127.5) $('pre code').addClass('shadow');
+      else $('pre code').removeClass('shadow')
+*/
+    }
+    document.add_or_hide_shadow = add_or_hide_shadow;
+
     var change_theme = function(theme) { 
       ((theme || '').length > 0) && $('#theme').attr('href', ['/css/', theme, '.css'].join('')) 
       $('#styles select').val(theme);
+      add_or_hide_shadow();
     }
+
     document.theme_switcher = setInterval(function() { change_theme(themes[themes.length.random()]) }, (10).seconds());
     
     var sorted_by_strlen = themes.map(function(s) { return s.length }).sort(function (a, b) { return (a < b)? -1: 1}),
@@ -39,6 +38,7 @@
   $('#styles').append(stylelist());
   $('select').on('change', function() { 
     $('#theme').attr('href', ['/css/', $(this).val(), '.css'].join('')) 
+    document.add_or_hide_shadow();
     document.theme_switcher && (function() { clearInterval(document.theme_switcher); delete theme_switcher; })(); //stop halt auto theme changer
   });
 
@@ -48,136 +48,94 @@
   $( document ).tooltip({
     tooltipClass:'tooltip'
   });
-
    
-/*
   (demo = function() {
 
-        //cascade example methods using send & reduce
+        //cascade example methods using <i>send</i> and <i>reduce</i>
 
     var build_demo = function(n, methods) { 
           return { 
-            method: [ "(", n, ").",methods.map(function(m) {return m + "()";}).join(".")].join('') + ';',
-            output: (methods.reduce(function(n, method) { return n.send(method)}, n) + "").replace(/(\d{2}:\d{2}:\d{2})/, "<em>$1</em>")
+            method: ["(", n, ").",
+                      methods.map(function(m) {
+                        return m + "()";
+                      }).join(".")
+                    ].join('') + ";",
+            output: (methods.reduce(function(n, method) { 
+              return n.send(method)
+            }, n) + "").replace(/(\d{2}:\d{2}:\d{2})/, "<em>$1</em>")
           }
         }, 
 
         //keep comments vertically aligned and easy to read 
+        //emulates printf "%42s" since the maximum cascaded method
+        //is 42 characters in length
 
         pad = function(n, s) {
-          return (n - s.length).map(function() { return ' ' }).join('');
+          return (n - s.length).map(function() { 
+            return ' ' 
+          }).join('');
         },
       
-        //with the two helpers above iterate through basic methods and cascade them with the 
-        //human friendly method <i> to_human </i>
+        //with the two helpers above, 
+        //iterate through date methods and cascade them with the 
+        //date calcuclations: <i> from_now </i> / <i> ago </i> 
+        //finished with the 
+        //readable date method <i> to_human </i>
 
-        recompute_timestamps = ["", "from_now", "ago"].map(function(time_method) {
-          return ["milliseconds", "seconds", "minutes", "hours", "days"].map(function(duration) { 
-             //return (time_method.length == 0)? build_demo((30).random(), [duration]) : build_demo((30).random(), [duration, time_method, 'to_human']);
-             return (time_method.length == 0)? build_demo(30, [duration]) : build_demo(30, [duration, time_method, 'to_human']);
-          });
+        recompute_timestamps = ["", 
+          "from_now", 
+          "ago"].map(function(time_method) {
+          return ["milliseconds", 
+                  "seconds", 
+                  "minutes", 
+                  "hours", 
+                  "days"].map(function(duration) { 
+                     return (time_method.length == 0)? 
+                       build_demo(30, [duration]) : 
+                       build_demo(30, [duration, time_method, 'to_human']);
+                  });
         }), 
 
         //build markup after flatting array generated above
 
-        markup = [].concat.apply([], recompute_timestamps).map(function(example, i) {
-          return [example.method, pad(42, example.method), "//&#8594; ", example.output, (((i+1)%5)==0)? "\n": ""].join("");
-        }).join("\n");
+        code_demo = [].
+                    concat.
+                    apply([], recompute_timestamps).
+                    map(function(example, i) {
+                      return [example.method, 
+                              pad(42, example.method), 
+                              "//&#8594; ", 
+                              example.output, 
+                              (((i+1)%5)==0)? "\n": ""].join("");
+                    }).join("\n");
     
-    //apply markup and reload syntax highlighting
+    //apply <em>code demo</em> and reload syntax highlighting
 
-    $($('pre code')[0]).html(["//Legible Date/Time calculations\n\n", markup].join(''));
-    hljs.highlightBlock($('pre code')[0]);
-    //$('code em').css('color', '#2ee');
+    $($('#live_example')[0]).html(
+      ["//Legible Date/Time calculations\n\n", 
+       code_demo].join('')
+    );
+    hljs.highlightBlock($('#live_example')[0]);
 
   })();
-  setInterval(demo, (1).second()); //reload example every second
+
+  setInterval(demo, (1).second()); //reload demo every second
 
   hljs.initHighlightingOnLoad();
-*/
-(demo = function() {
+  $('pre code').addClass('shadow');
 
-      //cascade example methods using <i>send</i> and <i>reduce</i>
-
-  var build_demo = function(n, methods) { 
-        return { 
-          method: ["(", n, ").",
-                    methods.map(function(m) {
-                      return m + "()";
-                    }).join(".")
-                  ].join('') + ";",
-          output: (methods.reduce(function(n, method) { 
-            return n.send(method)
-          }, n) + "").replace(/(\d{2}:\d{2}:\d{2})/, "<em>$1</em>")
-        }
-      }, 
-
-      //keep comments vertically aligned and easy to read 
-      //emulates printf "%42s" since the maximum cascaded method
-      //is 42 characters in length
-
-      pad = function(n, s) {
-        return (n - s.length).map(function() { 
-          return ' ' 
-        }).join('');
-      },
-    
-      //with the two helpers above, 
-      //iterate through date methods and cascade them with the 
-      //date calcuclations: <i> from_now </i> / <i> ago </i> 
-      //finished with the 
-      //readable date method <i> to_human </i>
-
-      recompute_timestamps = ["", 
-        "from_now", 
-        "ago"].map(function(time_method) {
-        return ["milliseconds", 
-                "seconds", 
-                "minutes", 
-                "hours", 
-                "days"].map(function(duration) { 
-                   return (time_method.length == 0)? 
-                     build_demo(30, [duration]) : 
-                     build_demo(30, [duration, time_method, 'to_human']);
-                });
-      }), 
-
-      //build markup after flatting array generated above
-
-      code_demo = [].
-                  concat.
-                  apply([], recompute_timestamps).
-                  map(function(example, i) {
-                    return [example.method, 
-                            pad(42, example.method), 
-                            "//&#8594; ", 
-                            example.output, 
-                            (((i+1)%5)==0)? "\n": ""].join("");
-                  }).join("\n");
-  
-  //apply <em>code demo</em> and reload syntax highlighting
-
-  $($('#live_example')[0]).html(
-    ["//Legible Date/Time calculations\n\n", 
-     code_demo].join('')
-  );
-  hljs.highlightBlock($('#live_example')[0]);
-
-})();
-setInterval(demo, (1).second()); //reload demo every second
-hljs.initHighlightingOnLoad();
-
-$('#release').on('click', function() {
-  var escapeHTML =function escapeHtml(string) {
-    return String(string).replace(/[&<>"'\/]/g, function (s) {
-      return {"&": "&amp;", "<": "&lt;", ">": "&gt;", '"': '&quot;', "'": '&#39;', "/": '&#x2F;' }[s];
+  $('#release').on('click', function() {
+    var escapeHTML =function escapeHtml(string) {
+      return String(string).replace(/[&<>"'\/]/g, function (s) {
+        return {"&": "&amp;", "<": "&lt;", ">": "&gt;", '"': '&quot;', "'": '&#39;', "/": '&#x2F;' }[s];
+      });
+    }
+    $.get('http://numberjs.dragonwrench.com/js/number.js', function(src) {
+      $('body').prepend(['<pre><code class="JavaScript">', escapeHTML(src), '</code></pre>'].join(''))
+      hljs.highlightBlock($('pre code')[0]);
+      $($('pre code')[0]).addClass('shadow');
     });
-  }
-  $.get('http://numberjs.dragonwrench.com/js/number.js', function(src) {
-    $('body').prepend(['<pre><code class="JavaScript">', escapeHTML(src), '</code></pre>'].join(''))
-    hljs.highlightBlock($('pre code')[0]);
   });
-});
 
 
 })();
